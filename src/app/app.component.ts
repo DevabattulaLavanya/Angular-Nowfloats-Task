a8797cf0fa08b4a473af394d472e697400456058
devalavanya <Lavanya.Devabattula@gmail.com>
2019-12-31 16:31:34 +0530

    Angular simple doctors Appointment dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  applicationinfo;
   title = 'NowfloatsTask';
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://api.myjson.com/bins/ftllh').subscribe(data => {
+      console.log(data);
+      this.applicationinfo = data;
+    });
+  }
 }
